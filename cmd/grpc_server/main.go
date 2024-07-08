f11a093cc473cb20969e0034bbaf0b4932eb1f3c
sovunia <burindima324@gmail.com>
2024-07-08 19:56:52 +0300

    add grpc handling

---
+++ cmd/grpc_server/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/brianvoe/gofakeit"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	desc "github.com/s0vunia/chat_microservices_course_boilerplate/pkg/chat_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const grpcPort = 50052
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
+// Create chat
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Create, usernames: %s", req.GetUsernames())
+
+	return &desc.CreateResponse{
+		Id: int64(gofakeit.Number(1, 100)),
+	}, nil
+}
+
+// Delete chat
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Delete id %d", req.GetId())
+
+	return nil, nil
+}
+
+// SendMessage to chat
+func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("Send message %s from %s", req.GetText(), req.GetFrom())
+
+	return nil, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
