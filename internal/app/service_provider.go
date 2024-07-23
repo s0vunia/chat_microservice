e9f26d47b2e4685013dc33e54cb55f337db82bc0
sovunia <burindima324@gmail.com>
2024-07-23 18:34:45 +0300

    add app

---
+++ internal/app/service_provider.go
@@ -0,0 +1,135 @@
+package app
+
+import (
+	"context"
+	"github.com/s0vunia/chat_microservices_course_boilerplate/internal/api/chat"
+	"github.com/s0vunia/chat_microservices_course_boilerplate/internal/client/db"
+	"github.com/s0vunia/chat_microservices_course_boilerplate/internal/client/db/pg"
+	"github.com/s0vunia/chat_microservices_course_boilerplate/internal/client/db/transaction"
+	"github.com/s0vunia/chat_microservices_course_boilerplate/internal/closer"
+	"github.com/s0vunia/chat_microservices_course_boilerplate/internal/config"
+	"github.com/s0vunia/chat_microservices_course_boilerplate/internal/repository"
+	chatRepository "github.com/s0vunia/chat_microservices_course_boilerplate/internal/repository/chat"
+	messageRepository "github.com/s0vunia/chat_microservices_course_boilerplate/internal/repository/message"
+	participantRepository "github.com/s0vunia/chat_microservices_course_boilerplate/internal/repository/participant"
+	"github.com/s0vunia/chat_microservices_course_boilerplate/internal/service"
+	chatService "github.com/s0vunia/chat_microservices_course_boilerplate/internal/service/chat"
+	"log"
+)
+
+type serviceProvider struct {
+	pgConfig   config.PGConfig
+	grpcConfig config.GRPCConfig
+
+	dbClient              db.Client
+	txManager             db.TxManager
+	chatRepository        repository.ChatRepository
+	messageRepository     repository.MessageRepository
+	participantRepository repository.ParticipantRepository
+
+	chatService service.ChatService
+
+	chatImpl *chat.Implementation
+}
+
+func newServiceProvider() *serviceProvider {
+	return &serviceProvider{}
+}
+
+func (s *serviceProvider) PGConfig() config.PGConfig {
+	if s.pgConfig == nil {
+		cfg, err := config.NewPGConfig()
+		if err != nil {
+			log.Fatalf("failed to get pg config: %s", err.Error())
+		}
+
+		s.pgConfig = cfg
+	}
+
+	return s.pgConfig
+}
+
+func (s *serviceProvider) GRPCConfig() config.GRPCConfig {
+	if s.grpcConfig == nil {
+		cfg, err := config.NewGRPCConfig()
+		if err != nil {
+			log.Fatalf("failed to get grpc config: %s", err.Error())
+		}
+
+		s.grpcConfig = cfg
+	}
+
+	return s.grpcConfig
+}
+
+func (s *serviceProvider) DBClient(ctx context.Context) db.Client {
+	if s.dbClient == nil {
+		cl, err := pg.New(ctx, s.PGConfig().DSN())
+		if err != nil {
+			log.Fatalf("failed to create db client: %v", err)
+		}
+
+		err = cl.DB().Ping(ctx)
+		if err != nil {
+			log.Fatalf("ping error: %s", err.Error())
+		}
+		closer.Add(cl.Close)
+
+		s.dbClient = cl
+	}
+
+	return s.dbClient
+}
+
+func (s *serviceProvider) TxManager(ctx context.Context) db.TxManager {
+	if s.txManager == nil {
+		s.txManager = transaction.NewTransactionManager(s.DBClient(ctx).DB())
+	}
+
+	return s.txManager
+}
+
+func (s *serviceProvider) ChatRepository(ctx context.Context) repository.ChatRepository {
+	if s.chatRepository == nil {
+		s.chatRepository = chatRepository.NewRepository(s.DBClient(ctx))
+	}
+
+	return s.chatRepository
+}
+
+func (s *serviceProvider) MessageRepository(ctx context.Context) repository.MessageRepository {
+	if s.messageRepository == nil {
+		s.messageRepository = messageRepository.NewRepository(s.DBClient(ctx))
+	}
+
+	return s.messageRepository
+}
+
+func (s *serviceProvider) ParticipantRepository(ctx context.Context) repository.ParticipantRepository {
+	if s.participantRepository == nil {
+		s.participantRepository = participantRepository.NewRepository(s.DBClient(ctx))
+	}
+
+	return s.participantRepository
+}
+
+func (s *serviceProvider) ChatService(ctx context.Context) service.ChatService {
+	if s.chatService == nil {
+		s.chatService = chatService.NewService(
+			s.ChatRepository(ctx),
+			s.MessageRepository(ctx),
+			s.ParticipantRepository(ctx),
+			s.TxManager(ctx),
+		)
+	}
+
+	return s.chatService
+}
+
+func (s *serviceProvider) ChatImpl(ctx context.Context) *chat.Implementation {
+	if s.chatImpl == nil {
+		s.chatImpl = chat.NewImplementation(s.ChatService(ctx))
+	}
+
+	return s.chatImpl
+}
