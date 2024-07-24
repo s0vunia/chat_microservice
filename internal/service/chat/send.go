853e6871d7e4c1ef10d24f1c4353a2d8652dae6c
sovunia <burindima324@gmail.com>
2024-07-24 11:50:40 +0300

    add custom err

--- internal/service/chat/send.go
+++ internal/service/chat/send.go
@@ -2,6 +2,7 @@ package chat
 
 import (
 	"context"
+	"github.com/pkg/errors"
 
 	"github.com/s0vunia/chat_microservices_course_boilerplate/internal/model"
 )
@@ -9,7 +10,7 @@ import (
 func (s *serv) SendMessage(ctx context.Context, createMessage *model.MessageCreate) (string, error) {
 	isExists, err := s.participantRepository.CheckParticipantInChat(ctx, createMessage.Info.ChatID, createMessage.Info.UserID)
 	if !isExists || err != nil {
-		return "", err
+		return "", errors.New("user is not a participant of the chat")
 	}
 	id, err := s.messageRepository.Send(ctx, createMessage)
 	if err != nil {
